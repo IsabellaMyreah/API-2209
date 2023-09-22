5698129bb262299041698eedef4060d3564e74c3
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-09-22 10:05:13 -0300

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+// Importando o framework express
+const express = require("express")
+const UserController = require("./controllers/UserController")
+
+// Importando o arquivo do UserController, que contém as funções de criar, buscar, atualizar e deletar os dados do usuário.
+
+// Inicializar as rotas
+const routes = express.Router()
+
+// Criando a rota /users utilizando o método post para cadastrar o usuário na aplicação/banco de dados.
+routes.post("/users", UserController.store)
+
+// Rota para buscar os dos dos usuários que foram cadastrados no banco de dados.
+routes.get("/users", UserController.index)
+
+
+
+routes.put("/users/:id", UserController.update)
+
+//Rota para deletar um registro na aplicação
+routes.delete("/users/:id", UserController.delete)
+
+
+routes.get("/users/:id", UserController.index2)
+
+module.exports = routes 
